637616194cb609ca2c44b1f38416e3100db3a24d
gruppjo <jona_blubb@web.de>
2018-12-11 02:05:19 +0100

    feat: replicate with svg as images

--- main.js
+++ main.js
@@ -1,15 +1,52 @@
 const input = document.querySelector('#input');
 const textContainer = document.querySelector('#text-container');
 
+const availableChars = {
+  a: {
+    flip: 'horizontal',
+  },
+  b: {
+    flip: 'vertical',
+  },
+  c: {
+    flip: 'vertical',
+  },
+  d: {
+    flip: 'vertical',
+  },
+  e: {
+    flip: 'vertical',
+  },
+  f: {
+    flip: false,
+  },
+  g: {
+    flip: false,
+  },
+  h: {
+    flip: 'horizontal',
+  },
+  i: {
+    flip: 'vertical',
+  },
+  j: {
+    flip: false,
+  }
+}
+
 input.addEventListener('input', () => {
   const value = input.value;
   console.log(value);
 
   newHtml = ``;
   for (let i = 0; i < value.length; i++) {
-    const currentChar = value[i];
+    const currentChar = value[i].toLowerCase();
+    const charDetails = availableChars[currentChar];
+    if (!charDetails) {
+		  return
+    }
     const charHtml = `
-      <div>${currentChar}</div>`;
+      <div class="character ${charDetails.flip}"><img src="./assets/System_Alphabet_${currentChar.toUpperCase()}.svg"></div>`;
       newHtml += charHtml;
   }
 
