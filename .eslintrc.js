6781a41b8d8a1906dbfb0a6f63d220d65ae217ef
gruppjo <jona_blubb@web.de>
2019-01-11 18:54:40 +0100

    setup: add react via cdn (https://reactjs.org/docs/add-react-to-a-website.html) and eslint & eslint-plugin-react

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "plugins": [
+    "react"
+  ],
+  "extends": ["eslint:recommended", "plugin:react/recommended"],
+  "env": {
+    "es6": true,
+    "browser": true
+  },
+  "globals": {
+    "React": true,
+    "ReactDOM": true
+  }
+}
---
+++ like_button.js
@@ -0,0 +1,32 @@
+'use strict';
+
+const e = React.createElement;
+
+class LikeButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    if (this.state.liked) {
+      return 'You liked this!';
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Like'
+    );
+  }
+}
+
+// Find all DOM containers, and render Like buttons into them.
+document.querySelectorAll('.like_button_container')
+  .forEach(domContainer => {
+    // Read the comment ID from a data-* attribute.
+    ReactDOM.render(
+      e(LikeButton),
+      domContainer
+    );
+  });
