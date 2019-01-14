a72a129127569a6c68a5ed89935a1189de400a9e
gruppjo <jona_blubb@web.de>
2019-01-14 20:36:56 +0100

    feat: old functionality in react (almost) | CharsService: properly fetch all chars and their info and allow to .bootstrap() and getCharJSX(), etc...

--- src/system/index.js
+++ src/system/index.js
@@ -1,87 +1,204 @@
 'use strict';
 
-const availableChars = {
-  a: {
-    flip: 'horizontal',
-  },
-  b: {
-    flip: 'vertical',
-  },
-  c: {
-    flip: 'vertical',
-  },
-  d: {
-    flip: 'vertical',
-  },
-  e: {
-    flip: 'vertical',
-  },
-  f: {
-    flip: false,
-  },
-  g: {
-    flip: false,
-  },
-  h: {
-    flip: 'horizontal',
-  },
-  i: {
-    flip: 'vertical',
-  },
-  j: {
-    flip: false,
-  },
-  k: {
-    flip: false,
-  },
-  l: {
-    flip: false,
-  },
-  m: {
-    flip: 'horizontal',
-  },
-  n: {
-    flip: 'horizontal-vertical'
-  },
-  o: {
-    flip: 'vertical'
-  },
-  p: {
-    flip: false
-  },
-  q: {
-    flip: false
-  },
-  r: {
-    flip: false
-  },
-  s: {
-    flip: 'vertical-horizontal'
-  },
-  t: {
-    flip: 'horizontal'
-  },
-  u: {
-    flip: 'horizontal'
-  },
-  v: {
-    flip: 'horizontal'
-  },
-  w: {
-    flip: 'horizontal'
-  },
-  x: {
-    flip: 'vertical'
-  },
-  y: {
-    flip: 'horizontal'
-  },
-  z: {
-    flip: 'vertical-horizontal'
+class CharsService {
+  // use
+  // .bootstrap
+  // .charData and
+  // .getCharJSX
+  constructor() {
+    this.availableCharsDetails = {
+      a: {
+        flip: 'horizontal',
+      },
+      b: {
+        flip: 'vertical',
+      },
+      c: {
+        flip: 'vertical',
+      },
+      d: {
+        flip: 'vertical',
+      },
+      e: {
+        flip: 'vertical',
+      },
+      f: {
+        flip: false,
+      },
+      g: {
+        flip: false,
+      },
+      h: {
+        flip: 'horizontal',
+      },
+      i: {
+        flip: 'vertical',
+      },
+      j: {
+        flip: false,
+      },
+      k: {
+        flip: false,
+      },
+      l: {
+        flip: false,
+      },
+      m: {
+        flip: 'horizontal',
+      },
+      n: {
+        flip: 'horizontal-vertical'
+      },
+      o: {
+        flip: 'vertical'
+      },
+      p: {
+        flip: false
+      },
+      q: {
+        flip: false
+      },
+      r: {
+        flip: false
+      },
+      s: {
+        flip: 'vertical-horizontal'
+      },
+      t: {
+        flip: 'horizontal'
+      },
+      u: {
+        flip: 'horizontal'
+      },
+      v: {
+        flip: 'horizontal'
+      },
+      w: {
+        flip: 'horizontal'
+      },
+      x: {
+        flip: 'vertical'
+      },
+      y: {
+        flip: 'horizontal'
+      },
+      z: {
+        flip: 'vertical-horizontal'
+      }
+    };
+    this.availableCharsJoined = Object.keys(this.availableCharsDetails).join('');
   }
-};
 
-const availableCharsJoined = Object.keys(availableChars).join('');
+  bootstrap() {
+    return this.fetchAllChars()
+    .then((charsData) => {
+      console.log('all loaded');
+      console.log(charsData);
+      this.charsData = {};
+      charsData.map((charData) => {
+        this.charsData[charData.char] = charData;
+      });
+    });
+  }
+
+  fetchAllChars() {
+    const allRequests = [];
+
+    // fetch all char files
+    for (let i = 0; i < this.availableCharsJoined.length; i++) {
+      const char = this.availableCharsJoined[i];
+      const charDetails = this.availableCharsDetails[char];
+      if (!charDetails) {
+        continue;
+      }
+
+      let request;
+      // fetch svg file as text
+      request = fetch(`./assets/System_Alphabet_${char.toUpperCase()}.svg`)
+      .then((response) => {
+        return response.text();
+      })
+      // make manipulations to svgText and return charData object
+      .then((svgText) => {
+        // make all ids unique otherwise defs and references interfere with each other
+        svgText = svgText.replace('SVGID', `SVGID${char}${i}`);
+        return {
+          svgText,
+          char,
+          charDetails
+        };
+      });
+      allRequests.push(request);
+    }
+
+
+    // make sure characters get processed in right order
+    return Promise.all(allRequests);
+  }
+
+  getCharJSX(char, duplicate, flip, invert) {
+    const charData = this.charsData[char];
+    // process svg as inline svg
+
+
+    // flip? - hack: always flip and on flip don't flip
+    // character svgs are improperly formatted
+    // mirrored characters
+    let svgHtml;
+    if (charData.charDetails.flip) {
+      svgHtml = `${invert ? charData.svgText : this.svgFlip(charData.svgText, charData.charDetails.flip)}`;
+      if (duplicate) {
+        svgHtml += `${!flip ? this.svgFlip(charData.svgText, charData.charDetails.flip) : charData.svgText}`;
+      }
+    }
+    // non mirrored characters
+    else {
+      svgHtml = `${charData.svgText}`;
+    }
+    const className = `character ${charData.charDetails.flip} ${!duplicate ? 'no-duplicate' : ''} ${charData.char}`;
+    return (
+      <div className={className} dangerouslySetInnerHTML={{__html:
+        svgHtml}}></div>
+    );
+  }
+
+  getCharInlineSVG (char, flip, invert) {
+    const charData = this.charsData[char];
+    if (!charData) {
+      console.warn(`CharsService.getCharInlineSVG() - character ${char} not available`);
+      return;
+    }
+
+    // non-mirrored characters
+    if (!charData.charDetails.flip) {
+      return `${charData.svgText}`;
+    }
+    // non-mirrored character
+    else {
+      return `${invert ? charData.svgText : this.svgFlip(charData.svgText, flip)}`;
+    }
+  }
+
+  svgFlip (svgText, flipDirection) {
+    const x = flipDirection.includes('horizontal') ? -1 : 1;
+    const y = flipDirection.includes('vertical') ? -1 : 1;
+    const style = `style="transform: scale(${x}, ${y})"`;
+    return svgText.replace('svg', `svg ${style}`);
+  }
+
+}
+
+const charsService = new CharsService();
+charsService.bootstrap()
+.then(() => {
+  const systemEl = document.querySelector('#system');
+  if (systemEl) {
+    ReactDOM.render(
+      <System/>,
+      systemEl
+    );
+  }
+});
 
 class System extends React.Component {
   render() {
@@ -113,7 +230,7 @@ class InputArea extends React.Component {
       });
     }
     // do nothing on these chars
-    if ( availableCharsJoined.indexOf(event.key.toLowerCase()) === -1 ) {
+    if ( charsService.availableCharsJoined.indexOf(event.key.toLowerCase()) === -1 ) {
       event.preventDefault();
 
     }
@@ -122,7 +239,8 @@ class InputArea extends React.Component {
   handleChange(event) {
     const value = event.target.value;
     this.setState({
-      value: value
+      value: value,
+
     });
 
     // print character
@@ -132,6 +250,10 @@ class InputArea extends React.Component {
   }
 
   render() {
+
+    const svgChars = this.state.value.split('').map((char) => {
+      return charsService.getCharJSX(char);
+    })
 
     return (
       <div>
@@ -150,16 +272,9 @@ class InputArea extends React.Component {
           onChange={this.handleChange}
         />
         <div id="text-container">
+          {svgChars}
         </div>
       </div>
     );
   }
-}
-
-const systemEl = document.querySelector('#system');
-if (systemEl) {
-  ReactDOM.render(
-    <System/>,
-    systemEl
-  );
 }
